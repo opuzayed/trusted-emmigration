880447da76bb5dd32ebe608e657f63e4b5ef1269
opuzayed <zayedopu4933@gmail.com>
2022-11-22 14:59:37 +0600

    navbar done

---
+++ src/components/shared/Header/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './Header.css';
+const Header = () => {
+  return (
+    <div className="navbar bg-info">
+      <div className="navbar-start">
+      <div className="w-10 rounded-full">
+          <img src="https://thumbs.dreamstime.com/b/world-travel-logo-vector-drawing-represents-design-40702766.jpg" />
+        </div>
+        <h2 className="btn btn-ghost normal-case text-3xl text-amber-500">
+          Trusted-Immigration
+        </h2>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal"> 
+           <Link to='/home'>Home</Link>
+           <Link to='/home'>Home</Link>
+           <Link to='/home'>Home</Link>
+           <Link to='/home'>Home</Link>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Get started</a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
