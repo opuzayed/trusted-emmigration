a063030ed0b30efb269aefb3cc231a41ef57385d
opuzayed <zayedopu4933@gmail.com>
2022-11-26 00:33:28 +0600

    context file done

---
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -0,0 +1,64 @@
+import { createContext, useEffect, useState } from "react";
+import app from "../../Firebase/firebase.config";
+import {
+  getAuth,
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const providerLogin = (provider) => {
+    setLoading(true);
+    return signInWithPopup(auth, provider);
+  };
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log("inside auth state change", currentUser);
+        setUser(currentUser);
+        setLoading(false);
+    });
+    return () => {
+     return unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    loading,
+    setLoading,
+    providerLogin,
+    logOut,
+    createUser,
+    signIn
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+export default AuthProvider;
